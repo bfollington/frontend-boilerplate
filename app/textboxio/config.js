203a56f1a7dc44c0409428e0c53468779e090818
Ben Follington <bfollington@gmail.com>
2016-01-29 14:06:00 +1000

    New textboxio config

--- app/textboxio/config.js
+++ app/textboxio/config.js
@@ -1,6 +1,36 @@
 
 export function defaultConfig() {
     return {
-        autosubmit: false
+        autosubmit: false,
+        paste: {
+            style: 'clean'
+        },
+        ui: {
+            toolbar: {
+                items: [
+                    'undo',
+                    'style',
+                    'emphasis',
+                    'align',
+                    'listindent',
+                    {
+                        id: 'insert',
+                        label: 'Insert',
+                        items: ['link', 'fileupload', 'table', 'hr', 'specialchar']
+                    },
+                    {
+                        label: 'format',
+                        items: [
+                            'removeformat',
+                            'superscript',
+                            'subscript',
+                            'strikethrough'
+                        ]
+                    },
+                    'tools',
+                    'language'
+                ]
+            }
+        }
     };
 }
